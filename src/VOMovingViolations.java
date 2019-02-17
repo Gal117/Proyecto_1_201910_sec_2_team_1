302e6f82479b63e4acfcc32fce2a20486d1850c4
Gal117 <danielgalindoruiz7@gmail.com>
2019-02-16 23:53:10 -0500

    VOMOving completado. Aun pendiente cargar.

--- src/VOMovingViolations.java
+++ src/VOMovingViolations.java
@@ -12,9 +12,9 @@ public class VOMovingViolations {
 	private String addressID;
 
 	/**
-	 * Atributo que da la fecha de la infracción
+	 * Atributo que da el identificador del segmento de calle donde hubo infraccion.
 	 */
-	private String ticketIssueDate;
+	private String streetSegID;
 
 	/**
 	 * Atributo que da el total a pagar por infracción (FINEAMT)
@@ -22,14 +22,29 @@ public class VOMovingViolations {
 	private int fineAMT;
 
 	/**
+	 * Atributo que da el dinero que efectivamente se pagó por infracción (TOALPAID)
+	 */
+	private int totalPaid;
+
+	/**
+	 * Atributo que da el dinero extra a pagar por infracción (penalty1)
+	 */
+	private int penalty1;
+
+	/**
+	 * Atributo que da el dinero extra a pagar por infracción (penalty2)
+	 */
+	private int penalty2;
+
+	/**
 	 * Atributo que da si hubo accidente o no
 	 */
 	private String accidentIndicator;
 
 	/**
-	 * Atributo que da la descripción de la infracción
+	 * Atributo que da la fecha de la infracción
 	 */
-	private String violationDescription;
+	private String ticketIssueDate;
 
 	/**
 	 * Atributo que da el tipo de infracción
@@ -37,91 +52,81 @@ public class VOMovingViolations {
 	private String violationCode;
 
 	/**
-	 * Atributo que da el dinero que efectivamente se pagó por infracción (TOALPAID)
+	 * Atributo que da la descripción de la infracción
 	 */
-	private int totalPaid;
-	
-	/**
-	 * Atributo que da el identificador del segmento de calle donde hubo infraccion.
-	 */
-	private String streetSegID;
-	
-	/**
-	 * Atributo que da el dinero extra a pagar por infracción (penalty1)
-	 */
-	private int penalty1;
-	
-	/**
-	 * Atributo que da el dinero extra a pagar por infracción (penalty2)
-	 */
-	private int penalty2;
-	
-	/**
-	 * Constructor
-	 * @param pId id infracción
-	 * @param pLoc ubicación infracción
-	 * @param pDate fecha infracción
-	 * @param pTotal total a pagar por infracción
-	 * @param pIndicator indicador de infracción
-	 * @param pDescription descripcion de infracción
-	 */
-	public VOMovingViolations(int pId, String pLoc, String pDate, int pTotal, String pIndicator, String pDescription){
+	private String violationDescription;
 
-		id= pId;
-		location = pLoc;
-		date = pDate;
-		total = pTotal;
-		accidentIndicator = pIndicator;
-		description = pDescription;
+	/**
+	 * Constructor VOMovingViolations
+	 * @param pObjectId identificador de la infracción
+	 * @param pIssueDate fecha de la infracción
+	 * @param pViolationCode tipo de infracción
+	 * @param pFineAMT dinero a pagar por la infracción
+	 * @param pAddress direccion de la infracción
+	 * @param pStreetSegId identificador de segmento de calle con infracción
+	 * @param pTotalPaid total que se pago efectivamente en la infracción
+	 * @param pDescrption descripcion de la infracción
+	 * @param pAccidentIndicator indica si hubo accidente o no en la infracción
+	 * @param pPenal1 extra por infracción
+	 * @param pPenal2 extra por infracción
+	 */
+	public VOMovingViolations(int pObjectId, String pIssueDate, String pViolationCode, int pFineAMT, String pAddress, String pStreetSegId, int pTotalPaid, String pDescrption, String pAccidentIndicator, int pPenal1, int pPenal2 ){
+		
+		objectID = pObjectId;
+		addressID = pAddress;
+		streetSegID = pStreetSegId;
+		fineAMT = pFineAMT;
+		totalPaid = pTotalPaid;
+		penalty1 = pPenal1;
+		penalty2 = pPenal2;
+		accidentIndicator = pAccidentIndicator;
+		ticketIssueDate = pIssueDate;
+		violationCode = pViolationCode;
+		violationDescription = pDescrption;
+
 	}
-	/**
-	 * @return id - Identificador Ãºnico de la infracciÃ³n
-	 */
-	public int objectId() {
-
-		return id;
-	}	
-
-
-	/**
-	 * @return location - DirecciÃ³n en formato de texto.
-	 */
-	public String getLocation() {
-
-		return location;
+	
+	public int darObjectID(){
+		return objectID;
+	}
+	
+	public String darDireccion(){
+		return addressID;
 	}
 
-	/**
-	 * @return date - Fecha cuando se puso la infracciÃ³n .
-	 */
-	public String getTicketIssueDate() {
-
-		return date;
+	public String darIDCalle(){
+		return streetSegID;
 	}
-
-	/**
-	 * @return FINEAMT - Cantidad a pagar po la infraccion en USD
-	 */
-	public int getFineAMT() {
-
-		return total;
+	
+	public int darFINEAMT(){
+		return fineAMT;
 	}
-
-	/**
-	 * @return accidentIndicator - Si hubo un accidente o no.
-	 */
-	public String  getAccidentIndicator() {
-
+	
+	public int darTotalPaid(){
+		return totalPaid;
+	}
+	
+	public int darPenal1(){
+		return penalty1;
+	}
+	
+	public int darPenal2(){
+		return penalty2;
+	}
+	
+	public String darAccidentIndicator(){
 		return accidentIndicator;
 	}
-
-	/**
-	 * @return description - DescripciÃ³n textual de la infracciÃ³n.
-	 */
-	public String  getViolationDescription() {
-
-		return description;
+	
+	public String darFecha(){
+		return ticketIssueDate;
 	}
-}
-
+	
+	public String darViolationCode(){
+		return violationCode;
+	}
+	
+	public String darDescripcion(){
+		return violationDescription;
+	}
 }
